26c8f36d6fd7856b9a58dad11441e2dc1a751cf8
sujinjinsu <121843966+sujinjinsu@users.noreply.github.com>
2023-06-18 22:38:15 +0900

    Update ApiKey.kt

--- Downloads/farmmate-rough-btmNav-yj/app/src/main/java/com/example/farmmate1/network/ApiKey.kt
+++ Downloads/farmmate-rough-btmNav-yj/app/src/main/java/com/example/farmmate1/network/ApiKey.kt
@@ -2,6 +2,6 @@ package com.example.farmmate1.network
 
 class ApiKey {
     companion object{
-        const val API_KEY="TF4qcEfT6ObRPZkI8XPooCNJDS39DY2HhujIugD%2BGc6NxharY00o5rb4baYR04I%2BoRwTK5wpVKjQoKf5tC9Vzw%3D%3D"
+        const val API_KEY="키 입력"
     }
 }
